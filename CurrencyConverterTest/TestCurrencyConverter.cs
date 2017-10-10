f15728b617adfee4bffa9fc5e4912b56e233e1ae
Michael Griffin <mdgriffin064@gmail.com>
2017-10-10 15:22:14 +0100

    Wrotes basic test to check GetExchangeRates method

---
+++ CurrencyConverterTest/TestCurrencyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyConverterTest
+{
+    [TestClass]
+    public class TestCurrencyConverter
+    {
+
+        [TestMethod]
+        public void TestGetChangeRates()
+        {
+            CurrencyConverterTest cc = new CurrencyConverter();
+
+            List<CurrencyModel> rates = cc.GetExchangeRates();
+
+
+            Assert.AreEqual(10, rates.length);
+        }
+    }
+}
