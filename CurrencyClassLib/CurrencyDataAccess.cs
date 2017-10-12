0b451aecbc937f5686353226be26fb3a596a0b30
Michael Griffin <mdgriffin064@gmail.com>
2017-10-12 17:10:47 +0100

    Added tests and stubs for data access layer

---
+++ CurrencyClassLib/CurrencyDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyClassLib
+{
+    public class CurrencyDataAccess
+    {
+
+        public String GetJSON ()
+        {
+            return "";
+        }
+
+
+        /*
+        public Rates GetRates (String baseCurrency)
+        {
+
+        }
+
+        public Rates GetRates(String baseCurrency, DateTime date)
+        {
+
+        }
+        */
+
+    }
+}
---
+++ CurrencyConverterTest/CurrencyDataAccessTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CurrencyClassLib;
+
+namespace CurrencyConverterTest
+{
+    [TestClass]
+    public class CurrencyDataAccessTest
+    {
+        [TestMethod]
+        public void TestGetJSON()
+        {
+
+            CurrencyDataAccess cd = new CurrencyDataAccess();
+
+            String jsonData = cd.GetJSON();
+
+            Assert.AreEqual(jsonData.Length, 0);
+        }
+    }
+}
