e78ffb4f39f67a53fdb2e3c5fca3c5ddacd146a1
Michael Griffin <mdgriffin064@gmail.com>
2017-10-31 11:04:53 +0000

    Performing currency conversion on frontend

--- CurrencyConverterWPF/ViewModels/ConversionFormViewModel.cs
+++ CurrencyConverterWPF/ViewModels/ConversionFormViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CurrencyClassLib;
 
 namespace CurrencyConverterFrontend.ViewModels
 {
@@ -14,15 +16,40 @@ namespace CurrencyConverterFrontend.ViewModels
         {
             this.parent = parent;
 
-            currency = new CurrencyModel();
+            currency = new CurrencyFormModel();
 
             btnText = "Convert!";
             btnHeading = "Convert Currencies";
         }
 
         // Properties accessible from the view
-        public CurrencyModel currency { get; set; }
+        public CurrencyFormModel currency { get; set; }
         public String btnHeading { get; set; }
         public String btnText { get; set; }
+
+        public ICommand convertCmd
+        {
+            get
+            {
+                return new RelayCommand(param => OnConvertClick(), param => true);
+            }
+        }
+
+
+        public void OnConvertClick ()
+        {
+            //currency.from
+            CurrencyExchanger cx = new CurrencyExchanger();
+
+            //cx.Convert(currency.from, currency.fromCode, currency.toCode);
+
+            if (!currency.HasErrors)
+            {
+                String fromCurrencyCode = currency.fromCode.Substring(currency.fromCode.Length - 4, 3);
+                String toCurrencyCode = currency.toCode.Substring(currency.toCode.Length - 4, 3);
+
+                currency.to = cx.Convert(Double.Parse(currency.from), fromCurrencyCode, toCurrencyCode).ToString();
+            }
+        }
     }
 }
