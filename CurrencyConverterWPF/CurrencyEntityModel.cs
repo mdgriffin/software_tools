7c5d61730d217726e88903fa262117219e4fd72f
Michael Griffin <mdgriffin064@gmail.com>
2017-10-31 09:50:42 +0000

    Renamed currentModel

---
+++ CurrencyConverterWPF/CurrencyEntityModel.cs
@@ -0,0 +1,36 @@
+namespace CurrencyConverterFrontend
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class CurrencyEntityModel : DbContext
+    {
+        public CurrencyEntityModel()
+            : base("name=CurrencyModel")
+        {
+        }
+
+        public virtual DbSet<Conversion> Conversions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Conversion>()
+                .Property(e => e.SourceCurrency)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Conversion>()
+                .Property(e => e.DestinationCurrency)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Conversion>()
+                .Property(e => e.Amount)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Conversion>()
+                .Property(e => e.ConvertedAmount)
+                .HasPrecision(19, 4);
+        }
+    }
+}
