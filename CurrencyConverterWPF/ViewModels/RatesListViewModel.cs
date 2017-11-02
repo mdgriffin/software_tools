8398e93528db5a88b5d7f52b4428d31a03605c42
Michael Griffin <mdgriffin064@gmail.com>
2017-11-02 11:19:46 +0000

    Added method to change selected currency

--- CurrencyConverterWPF/ViewModels/RatesListViewModel.cs
+++ CurrencyConverterWPF/ViewModels/RatesListViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CurrencyClassLib;
+using System.Windows;
 
 namespace CurrencyConverterFrontend.ViewModels
 {
@@ -14,19 +15,40 @@ namespace CurrencyConverterFrontend.ViewModels
         {
             this.parent = parent;
 
-            CurrencyExchanger cx = new CurrencyExchanger();
-
-            rates = cx.GetExchangeRatesList();
+            cx = new CurrencyExchanger();
 
             txtHeading = "Euro Rates";
-
             baseCurrency = "Euro (EUR)";
         }
+
+        private CurrencyExchanger cx;
 
         // Properties accessible from the view
         public Rates rates { get; set; }
         public String txtHeading { get; set; }
 
-        public String baseCurrency { get; set; }
+        private String _baseCurrency;
+        public String baseCurrency {
+            get
+            {
+                return _baseCurrency;
+            }
+            set
+            {
+                if (value != baseCurrency)
+                {
+                    _baseCurrency = value;
+                    NotifyPropertyChanged("baseCurrency");
+                    changeCurrencyList();
+                }
+                
+            }
+        }
+
+        private void changeCurrencyList ()
+        {
+            String baseCurrencyCode = baseCurrency.Substring(baseCurrency.Length - 4, 3);
+            rates = cx.GetExchangeRatesList(baseCurrencyCode);
+        }
     }
 }
