12d2022723fc16fd15cbd4b970aacda8d4fed450
Michael Griffin <mdgriffin064@gmail.com>
2017-11-07 20:09:55 +0000

    Fixed issues with time series data access

--- CurrencyClassLib/TimeSeriesDataAccess.cs
+++ CurrencyClassLib/TimeSeriesDataAccess.cs
@@ -9,21 +9,21 @@ namespace CurrencyClassLib
 {
     public class TimeSeriesDataAccess
     {
-        private const String API_KEY = "7KWFxr5nhMn4NDm4jtNC";
         private const String BASE_URL = "https://www.quandl.com/api/v3/datasets/ECB/";
+        private const String API_KEY = "7KWFxr5nhMn4NDm4jtNC";
 
-        public static String GetJSON(String baseCurrencyCode, String destinationCurrencyCode)
+        public static String GetJSON(String currencyCode)
         {
-            String arguments = String.Format(
-                "{0}{1}?start_date={2}&end_date={3}&collapse=weekly&api_key={4}",
-                baseCurrencyCode,
-                destinationCurrencyCode,
+            String fullUrl = String.Format(
+                "{0}EUR{1}?start_date={2}&end_date={3}&collapse=weekly&api_key={4}",
+                BASE_URL,
+                currencyCode,
                 "01-05-2005",
                 "01-05-2006",
                 API_KEY
             );
 
-            var json = new WebClient().DownloadString(BASE_URL + arguments);
+            var json = new WebClient().DownloadString(fullUrl);
 
             return json;
         }
---
+++ CurrencyConverterTest/TimeSeriesDataAccessTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CurrencyClassLib;
+
+namespace CurrencyConverterTest
+{
+    [TestClass]
+    public class TimeSeriesDataAccessTest
+    {
+        [TestMethod]
+        public void TestGetJSON()
+        {
+            String jsonData = TimeSeriesDataAccess.GetJSON("USD");
+
+            Assert.IsTrue(jsonData.Length > 0);
+        }
+    }
+}
