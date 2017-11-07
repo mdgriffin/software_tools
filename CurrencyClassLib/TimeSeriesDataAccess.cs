4632b6382eb287524cba84cd779bf5aacdf8706c
Michael Griffin <mdgriffin064@gmail.com>
2017-11-07 22:13:45 +0000

    Added start and end date parameters

--- CurrencyClassLib/TimeSeriesDataAccess.cs
+++ CurrencyClassLib/TimeSeriesDataAccess.cs
@@ -12,14 +12,14 @@ namespace CurrencyClassLib
         private const String BASE_URL = "https://www.quandl.com/api/v3/datasets/ECB/";
         private const String API_KEY = "7KWFxr5nhMn4NDm4jtNC";
 
-        public static String GetJSON(String currencyCode)
+        public static String GetJSON(String currencyCode, DateTime startDate, DateTime endDate)
         {
             String fullUrl = String.Format(
                 "{0}EUR{1}.json?start_date={2}&end_date={3}&collapse=weekly&api_key={4}",
                 BASE_URL,
                 currencyCode,
-                "01-05-2005",
-                "01-05-2006",
+                startDate.ToString("dd-MM-yyyy"),
+                endDate.ToString("dd-MM-yyyy"),
                 API_KEY
             );
 
--- CurrencyConverterTest/TimeSeriesDataAccessTest.cs
+++ CurrencyConverterTest/TimeSeriesDataAccessTest.cs
@@ -10,7 +10,7 @@ namespace CurrencyConverterTest
         [TestMethod]
         public void TestGetJSON()
         {
-            String jsonData = TimeSeriesDataAccess.GetJSON("USD");
+            String jsonData = TimeSeriesDataAccess.GetJSON("USD", DateTime.ParseExact("25-04-2005", "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture), DateTime.ParseExact("25-04-2006", "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture));
 
             Assert.IsTrue(jsonData.Length > 0);
         }
