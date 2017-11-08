bff9021a1377c3ab977282458015079611b8da83
Michael Griffin <mdgriffin064@gmail.com>
2017-11-08 08:27:39 +0000

    Added models for currency time series

---
+++ CurrencyClassLib/DataSetModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyClassLib
+{
+    public class DataSetModel
+    {
+        public string Collapse { get; set; }
+        public object ColumnIndex { get; set; }
+        public string[] ColumnNames { get; set; }
+        public Data[][] Data { get; set; }
+        public string DatabaseCode { get; set; }
+        public long DatabaseId { get; set; }
+        public string DatasetCode { get; set; }
+        public string Description { get; set; }
+        public string EndDate { get; set; }
+        public string Frequency { get; set; }
+        public long Id { get; set; }
+        public object Limit { get; set; }
+        public string Name { get; set; }
+        public string NewestAvailableDate { get; set; }
+        public string OldestAvailableDate { get; set; }
+        public object Order { get; set; }
+        public bool Premium { get; set; }
+        public string RefreshedAt { get; set; }
+        public string StartDate { get; set; }
+        public object Transform { get; set; }
+        public string Type { get; set; }
+    }
+
+    public struct Data
+    {
+        public double? Double;
+        public string String;
+    }
+}
