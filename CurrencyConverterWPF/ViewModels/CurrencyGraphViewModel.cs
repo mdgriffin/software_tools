6e0d651484ce24f8498e2cbce2ff23ec6837b4db
Michael Griffin <mdgriffin064@gmail.com>
2017-11-08 17:54:22 +0000

    Changing graph on combobox change

--- CurrencyConverterWPF/ViewModels/CurrencyGraphViewModel.cs
+++ CurrencyConverterWPF/ViewModels/CurrencyGraphViewModel.cs
@@ -16,29 +16,66 @@ namespace CurrencyConverterFrontend.ViewModels
         {
             this.parent = parent;
 
-            txtHeading = "Currency Graph";
+            // Set the default base currency code
+            selectedCurrency = "US Dollar (USD)";
 
+            // Set the default heading
+            txtHeading = "Dollar Rates";
+        }
+
+        public String txtHeading { get; set; }
+        private String _selectedCurrency;
+        public String selectedCurrency {
+            get
+            {
+                return _selectedCurrency;
+            }
+            set
+            {
+                if (value != _selectedCurrency)
+                {
+                    _selectedCurrency = value;
+                    NotifyPropertyChanged("selectedCurrency");
+                    generateGraph();
+                }
+            }
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        public ChartValues<double> chartValues { get; set; }
+        public String[] chartLabels { get; set; }
+
+
+        public void generateGraph ()
+        {
             DateTime startTime = DateTime.Parse("15-04-2005");
             DateTime endTime = DateTime.Parse("15-04-2006");
+
+            List<List<Object>> currencyTimeSeries = TimeSeries.GetTimeSeries(selectedCurrency.Substring(selectedCurrency.Length - 4, 3), startTime, endTime);
+
+            if (chartLabels == null || chartValues == null)
+            {
+                chartLabels = new String[currencyTimeSeries.Count];
+                chartValues = new ChartValues<double>();
+            }
             
-
-            List<List<Object>> currencyTimeSeries = TimeSeries.GetTimeSeries("USD", startTime, endTime);
-
-            ChartValues<double> chartValues = new ChartValues<double>();
-            String[] chartLabels = new String[currencyTimeSeries.Count];
             int index = 0;
+
+            chartValues.Clear();
 
             foreach (List<Object> timePoint in currencyTimeSeries)
             {
-                chartLabels[index++] = (String)timePoint[0];
-                chartValues.Add((double)timePoint[1]);
+                chartLabels[index] = (String)timePoint[0];
+                chartValues.Insert(index++, (double)timePoint[1]);
+                
             }
 
             SeriesCollection = new SeriesCollection
             {
                 new LineSeries
                 {
-                    Title = "USD",
+                    Title = selectedCurrency,
                     Values = chartValues
                 },
             };
@@ -46,12 +83,7 @@ namespace CurrencyConverterFrontend.ViewModels
             Labels = chartLabels;
 
 
-            //YFormatter = value => value.ToString("C");
+            YFormatter = value => value.ToString("C");
         }
-
-        public String txtHeading { get; set; }
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        //public Func<double, string> YFormatter { get; set; }
     }
 }
