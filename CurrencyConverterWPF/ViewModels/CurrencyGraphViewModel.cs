d4ba5d65c71a2ee2a69102832097b25853b04ca9
Michael Griffin <mdgriffin064@gmail.com>
2017-11-09 12:29:06 +0000

    Added date pickers for selecting start and ends dates
    Added min and max dates to ensure date can only be within a certain time frame

--- CurrencyConverterWPF/ViewModels/CurrencyGraphViewModel.cs
+++ CurrencyConverterWPF/ViewModels/CurrencyGraphViewModel.cs
@@ -21,6 +21,15 @@ namespace CurrencyConverterFrontend.ViewModels
 
             // Set the default heading
             txtHeading = "Dollar Rates";
+
+            // The minimum and maximum dates that can be selected
+            dateRangeMin = "1/01/2000";
+            dateRangeMax = DateTime.Today.ToString("M/dd/yyyy");
+
+            // The default date range from 1 year ago to today
+            dateRangeStart = DateTime.Today.AddYears(-1);
+            dateRangeEnd = DateTime.Today;
+
         }
 
         public String txtHeading { get; set; }
@@ -45,14 +54,15 @@ namespace CurrencyConverterFrontend.ViewModels
         public Func<double, string> YFormatter { get; set; }
         public ChartValues<double> chartValues { get; set; }
         public String[] chartLabels { get; set; }
+        public String dateRangeMin { get; set; }
+        public String dateRangeMax { get; set; }
+        public DateTime dateRangeStart { get; set; }
+        public DateTime dateRangeEnd { get; set; }
 
 
         public void generateGraph ()
         {
-            DateTime startTime = DateTime.Parse("15-04-2005");
-            DateTime endTime = DateTime.Parse("15-04-2006");
-
-            List<List<Object>> currencyTimeSeries = TimeSeries.GetTimeSeries(selectedCurrency.Substring(selectedCurrency.Length - 4, 3), startTime, endTime);
+            List<List<Object>> currencyTimeSeries = TimeSeries.GetTimeSeries(selectedCurrency.Substring(selectedCurrency.Length - 4, 3), dateRangeStart, dateRangeEnd);
 
             if (chartLabels == null || chartValues == null)
             {
